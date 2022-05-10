f00ae4d14349fc2a7e28da417770bcbcdeb16186
OrhanDulancic <orhan.dulancic@outlook.com>
2022-05-10 19:22:48 +0200

    Intro JS

---
+++ fundamentals1/src/script.js
@@ -0,0 +1,154 @@
+/* First steps, const let (variables) boolean, data types , 1# Coding Example */
+
+// 1. First steps in js !!! (linking html with script, difference between inline and external js file)
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('AkmeSr');
+// console.log(23);
+
+// let firstName = 'Orhan'; // Declaring a variable!
+// console.log(firstName);
+// let PI = 3.1415;
+// let person = 'orhan';
+// let myCurrentJob = 'Sef servisa';
+
+// let country = 'Bosnia and Herzegowina';
+// let continent = 'Europe';
+// let population = '2 million';
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'orhan');
+
+// javascriptIsFun = 'YES';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(year);
+// console.log(typeof year);
+
+// console.log(typeof null); // bug returns OBJECT
+
+// let isIsland = false;
+// let language;
+// let country = "Bosnia and Herzegowina";
+// let population = "2 million";
+
+// console.log(typeof isIsland);
+// console.log(typeof language);
+// console.log(typeof country);
+// console.log(typeof population);
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+
+// var job = "Genelec";
+// job = "Programmer";
+
+// lastName = "Dulancic";
+// console.log(lastName);
+
+// let language = "Bosnian";
+// language = "Serbian";
+// const country = "Bosnia";
+// language = "Serbian";
+
+// country = "Serbia";
+
+// console.log(language);
+
+// const now = 2022;
+// const ageOrhan = now - 1994;
+// const ageTiki = now - 2018;
+// console.log(ageOrhan, ageTiki);
+
+// console.log(ageOrhan * 2, ageOrhan / 2, 2 ** 3);
+// // 2**3 means 2 to the power of 3 = 2*2*2
+
+// const firstName = "Orhan";
+// const lastName = "Dulancic";
+// console.log(firstName + " " + lastName);
+
+// let x = 10+5 ; // 15
+// x += 10 ; // x = x +10
+// x--;
+// x--;
+// console.log(x);
+
+// Comparison operators
+// console.log(ageOrhan > ageTiki); //true
+// console.log(ageTiki >= 18);
+
+// const isFullAge = ageTiki >= 18;
+// console.log(now - 1991 > now - 2018);
+
+// let countryPopulation = 2000000;
+// console.log(countryPopulation / 2);
+
+// countryPopulation++;
+// console.log(countryPopulation);
+
+// let finlandPopulation = 6000000;
+
+// console.log(countryPopulation > finlandPopulation);
+
+// const averagePopCountry = 33000000;
+
+// console.log(averagePopCountry > countryPopulation);
+// let language = "Bosnian"
+// const country = "Bosnia"
+// let continent = 'Europe';
+
+// let descriptions = country + ' is in ' + continent + ', ' + language; 
+// console.log(descriptions);
+
+// const now = 2037;
+// const ageOrhan = now -1991;
+// const ageTiki = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+// let x, y;
+// x= y = 25 - 10 - 5;
+// console.log(x,y);
+// const avarageAge = (ageOrhan + ageTiki) / 2;
+// console.log(ageOrhan, ageTiki, avarageAge);
+
+// Coding challenge #1
+
+// const markMass = 78;
+// const markHeight = 1.69; 
+
+// const johnMass = 92 ;
+// const johnHeight = 1.95;
+
+
+
+
+// let bmiMark = markMass / (markHeight ** 2);
+// console.log(bmiMark);
+
+// let bmiJohn = johnMass / (johnHeight **2);
+// console.log(bmiJohn);
+
+// let markHigherBmi = bmiJohn > bmiMark;
+// console.log(markHigherBmi);
+
+
+
+
